468277adc1a05b8845742529690fbdf7454d4617
Murp01 <pmurphy35@hotmail.com>
2021-05-06 06:43:19 +0100

    Added page objects and locator classes.  Base page etc

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -6,25 +6,25 @@ use_step_matcher('re')
 
 @given('I am on the homepage')
 def step_impl(context):
-    context.browser = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
+    context.driver = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
     #browser = webdriver.Chrome('chromedriver.exe')
-    context.browser.get('http:127.0.0.1:5000')
+    context.driver.get('http:127.0.0.1:5000')
     #context.browser.get('https://www.google.com')
 
 
 @given('I am on the blog page')
 def step_impl(context):
-    context.browser = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
-    context.browser.get('http:127.0.0.1:5000/blog')
+    context.driver = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
+    context.driver.get('http:127.0.0.1:5000/blog')
 
 
 @then('I am on the blog page')
 def step_impl(context):
     expected_url = 'http://127.0.0.1:5000/blog'
-    assert context.browser.current_url == expected_url
+    assert context.driver.current_url == expected_url
 
 
 @then('I am on the homepage')
 def step_impl(context):
     expected_url = 'http://127.0.0.1:5000/'
-    assert context.browser.current_url == expected_url
+    assert context.driver.current_url == expected_url
