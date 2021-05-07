1d51f6fa95b135cc9889386cd5fa489d266530b4
Murp01 <pmurphy35@hotmail.com>
2021-05-07 06:47:53 +0100

    Setting up the Blog page

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -2,29 +2,34 @@ from behave import *
 from selenium import webdriver
 import chromedriver_binary
 
+from tests.acceptance.page_model.blog_page import BlogPage
+from tests.acceptance.page_model.home_page import HomePage
+
 use_step_matcher('re')
 
 @given('I am on the homepage')
 def step_impl(context):
     context.driver = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
     #browser = webdriver.Chrome('chromedriver.exe')
-    context.driver.get('http:127.0.0.1:5000')
-    #context.browser.get('https://www.google.com')
+    #context.driver.get('https://www.google.com')
+    page = HomePage(context.driver)
+    context.driver.get(page.url)
 
 
 @given('I am on the blog page')
 def step_impl(context):
     context.driver = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
+    page = BlogPage(context.driver)
     context.driver.get('http:127.0.0.1:5000/blog')
 
 
 @then('I am on the blog page')
 def step_impl(context):
-    expected_url = 'http://127.0.0.1:5000/blog'
+    expected_url = BlogPage(context.driver).url
     assert context.driver.current_url == expected_url
 
 
 @then('I am on the homepage')
 def step_impl(context):
-    expected_url = 'http://127.0.0.1:5000/'
+    expected_url = HomePage(context.driver).url
     assert context.driver.current_url == expected_url
