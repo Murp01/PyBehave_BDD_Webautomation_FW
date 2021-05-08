20b4a115459bf052482744141e676f0766f20385
Murp01 <pmurphy35@hotmail.com>
2021-05-08 20:37:06 +0100

    Using pages in navigation

--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -1,15 +1,18 @@
 from behave import *
 
 from tests.acceptance.page_model.base_page import BasePage
+from tests.acceptance.locators.base_page import *
 
 use_step_matcher('re')
+
 
 @then('there is a title shown on the page')
 def step_impl(context):
     page = BasePage(context.driver)
     assert page.title.is_displayed()
 
+
 @then('the title tag has content "(.*)"')
 def step_impl(context, content):
     page = BasePage(context.driver)
-    assert page.title_tag.text == content
+    assert page.title.text == content
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -20,7 +20,7 @@ def step_impl(context):
 def step_impl(context):
     context.driver = webdriver.Chrome(executable_path=r"C:\Users\pmurp\PycharmProjects\video_code\tests\acceptance\steps\chromedriver.exe");
     page = BlogPage(context.driver)
-    context.driver.get('http:127.0.0.1:5000/blog')
+    context.driver.get(page.url)
 
 
 @then('I am on the blog page')
