3ee4a4197340862d86fbb092ee0e14de9761d555
Murp01 <pmurphy35@hotmail.com>
2021-05-09 17:39:32 +0100

    Waits and timeouts in Selenium

--- tests/acceptance/content.feature
+++ tests/acceptance/content.feature
@@ -10,3 +10,8 @@ Feature: Test that pages have the correct content
     Given I am on the homepage
     Then there is a title shown on the page
     And the title tag has content "This is the homepage"
+
+  Scenario: Blog page loads the posts
+    Given I am on the blog page
+    And I wait for the posts to load
+    Then I can see there is a posts section on the page
--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -2,6 +2,7 @@ from behave import *
 
 from tests.acceptance.page_model.base_page import BasePage
 from tests.acceptance.locators.base_page import *
+from tests.acceptance.page_model.blog_page import BlogPage
 
 use_step_matcher('re')
 
@@ -16,3 +17,9 @@ def step_impl(context):
 def step_impl(context, content):
     page = BasePage(context.driver)
     assert page.title.text == content
+
+
+@then('I can see there is a posts section on the page')
+def step_impl(context):
+    page = BlogPage(context.driver)
+    assert page.posts_section.is_displayed()
